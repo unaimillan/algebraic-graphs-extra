f0012cd3d34eeda86292f0b727c8e6e66714c4e5
Dantara <supercreed2@gmail.com>
2020-06-02 19:18:23 +0300

    Init

--- src/Algebra/Graph/Algorithm.hs
+++ src/Algebra/Graph/Algorithm.hs
@@ -1,3 +1,45 @@
 module Algebra.Graph.Algorithm where
 
+import Algebra.Graph
+
+shortestPath :: Eq a => Graph a -> a -> a -> Maybe (Int, [a])
+
+shortestPath Empty _ _ = Nothing
+
+shortestPath (Vertex x) _ t
+  | x == t = Just (1, [x])
+  | otherwise = Nothing
+
+shortestPath (Connect x y) s t =
+  case (path_x, path_y) of
+    (Just (n1, p1), Just (n2, p2)) ->
+      Just (connHelper (n1, p1) (n2, p2))
+    (Just (n1, p1), Nothing) ->
+      Just (n1+1, p1)
+    (Nothing, Just (n2, p2)) ->
+      Just (n2+1, p2)
+    _ ->
+      Nothing
+  where
+    path_x = shortestPath x s t
+    path_y = shortestPath y s t
+
+shortestPath (Overlay x y) s t =
+  case (path_x, path_y) of
+    (Just (n1, p1), Just (n2, p2)) ->
+      Just (connHelper (n1, p1) (n2, p2))
+    (Just (n1, p1), Nothing) ->
+      Just (n1+1, p1)
+    (Nothing, Just (n2, p2)) ->
+      Just (n2+1, p2)
+    _ ->
+      Nothing
+  where
+    path_x = shortestPath x s t
+    path_y = shortestPath y s t
+
+connHelper :: (Int, [a]) -> (Int, [a]) -> (Int, [a])
+connHelper (n1, p1) (n2, p2)
+  | n1 > n2 = (n1 + 1, p1)
+  | otherwise = (n2 + 1, p2)
 
--- src/MyProject.hs
+++ src/MyProject.hs
@@ -1,4 +1,14 @@
 module MyProject where
 
+import Algebra.Graph
+import Algebra.Graph.Algorithm
+
+sampleGraph :: Graph Int
+sampleGraph = Overlay
+  (Connect (Vertex 1) (Vertex 2))
+  (Connect (Vertex 2) (Vertex 3))
+
 run :: IO ()
 run = putStrLn "Hello, world!"
+
+test = shortestPath sampleGraph 1 3
