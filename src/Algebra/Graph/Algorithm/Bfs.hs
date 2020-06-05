35f791eec02e89a9ed62e1ded0e3429df7e8bca1
Dantara <supercreed2@gmail.com>
2020-06-05 16:59:41 +0300

    Visited: List to Set

--- src/Algebra/Graph/Algorithm/Bfs.hs
+++ src/Algebra/Graph/Algorithm/Bfs.hs
@@ -1,49 +1,53 @@
 module Algebra.Graph.Algorithm.Bfs(bfs) where
 
 import Algebra.Graph
+import qualified Data.Set as S
 
+-- Extracts all vertices values from given graph
 extractVertices :: Graph a -> [a]
 extractVertices Empty = []
 extractVertices (Vertex x) = [x]
 extractVertices (Connect x y) = (extractVertices x) <> (extractVertices y)
 extractVertices (Overlay x y) = (extractVertices x) <> (extractVertices y)
 
-connectedWithHelper :: Eq a => Graph a -> a -> Maybe [a]
-connectedWithHelper Empty _ = Nothing
-connectedWithHelper (Vertex _) _ = Nothing
-connectedWithHelper (Connect (Vertex x) (Vertex y)) t
+-- Finds all vertices which connected with given point.
+-- Returns Nothing if point wasn't found in the graph.
+-- Returns Just [a] (where [a] is list of points) otherwise.
+connectedWith :: Ord a => Graph a -> a -> Maybe [a]
+connectedWith Empty _ = Nothing
+connectedWith (Vertex _) _ = Nothing
+connectedWith (Connect (Vertex x) (Vertex y)) t
   | x == t = Just [y]
   | y == t = Just []
   | otherwise = Nothing
-connectedWithHelper (Connect (Vertex x) y) t
+connectedWith (Connect (Vertex x) y) t
   | x == t = Just (extractVertices y)
-  | otherwise = connectedWithHelper y t
-connectedWithHelper (Connect x y) t = Just (left <> right)
+  | otherwise = connectedWith y t
+connectedWith (Connect x y) t = Just (left <> right)
   where
-    left = unwrapMaybeList $ (<>) <$> (connectedWithHelper x t) <*> Just (extractVertices y)
-    right = unwrapMaybeList $ connectedWithHelper y t
-connectedWithHelper (Overlay x y) t = (connectedWithHelper x t) <> (connectedWithHelper y t)
-
-connectedWith :: Eq a => Graph a -> a -> [a]
-connectedWith graph t = unwrapMaybeList $ connectedWithHelper graph t
+    left = unwrapMaybeList $ (<>) <$> (connectedWith x t) <*> Just (extractVertices y)
+    right = unwrapMaybeList $ connectedWith y t
+connectedWith (Overlay x y) t = (connectedWith x t) <> (connectedWith y t)
 
 unwrapMaybeList :: Maybe [a] -> [a]
 unwrapMaybeList Nothing = []
 unwrapMaybeList (Just x) = x
 
-bfsDepth :: Eq a => Graph a -> a -> Int -> [(a, Int)]
-bfsDepth graph t d = (\x -> (x, d)) <$> (connectedWith graph t)
+bfsDepth :: Ord a => Graph a -> a -> Int -> [(a, Int)]
+bfsDepth graph t d = (\x -> (x, d)) <$> (unwrapMaybeList $ connectedWith graph t)
 
-bfsLoop :: Eq a => Graph a -> a -> [(a, Int)] -> [a] -> Int -> [(a, Int)]
+-- The body of bfs algorithm
+bfsLoop :: Ord a => Graph a -> a -> [(a, Int)] -> S.Set a -> Int -> [(a, Int)]
 bfsLoop _ _ [] _ _ = []
 bfsLoop graph s (x:xs) visited depth
-  | (notElem p visited) && (s /= p) = x : bfsLoop graph s (xs <> conns) (p:visited) (depth + 1)
-  | otherwise = bfsLoop graph s xs (p:visited) (depth + 1)
+  | (S.notMember p visited) && (s /= p) = x : bfsLoop graph s (xs <> conns) (S.insert p visited) (depth + 1)
+  | otherwise = bfsLoop graph s xs visited (depth + 1)
   where
     conns = bfsDepth graph p (depth + 1)
     (p, _) = x
 
-bfs :: Eq a => Graph a -> a -> [(a, Int)]
-bfs graph s = bfsLoop graph s initial [] 1
+-- The main Breadth-first search function
+bfs :: Ord a => Graph a -> a -> [(a, Int)]
+bfs graph s = bfsLoop graph s initial S.empty 1
   where
     initial = bfsDepth graph s 1
--- src/MyProject.hs
+++ src/MyProject.hs
@@ -16,17 +16,5 @@ sampleGraph2 = Connect
 run :: IO ()
 run = putStrLn "Hello, world!"
 
--- test :: Maybe (Int, [Int])
--- test = shortestPath sampleGraph 1 4
-
--- testFindConnect :: Int -> [Graph Int]
--- testFindConnect x = findConnect sampleGraph x
-
--- testConnectedWith :: Int -> [Int]
--- testConnectedWith x = connectedWith sampleGraph2 x
-
--- testBfsInit :: Int -> [(Int, Int)]
--- testBfsInit x = bfsInit sampleGraph x
-
 testBfs :: Int -> [(Int, Int)]
-testBfs x = bfs sampleGraph2 x
+testBfs x = bfs sampleGraph x
