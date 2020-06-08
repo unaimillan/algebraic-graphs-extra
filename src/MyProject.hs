145d58f1172f8f81113148ec415d2e3b97c62809
Dantara <supercreed2@gmail.com>
2020-06-08 21:36:29 +0300

    Specs

--- src/MyProject.hs
+++ src/MyProject.hs
@@ -1,20 +1,4 @@
 module MyProject where
 
-import Algebra.Graph
-import Algebra.Graph.Algorithm
-
-sampleGraph :: Graph Int
-sampleGraph = Overlay
-  (Connect (Vertex 1) (Vertex 2))
-  (Connect (Vertex 2) (Connect (Vertex 3) (Vertex 4)))
-
-sampleGraph2 :: Graph Int
-sampleGraph2 = Connect
-  (Connect (Vertex 1) (Vertex 2))
-  (Overlay (Vertex 3) (Vertex 4))
-
 run :: IO ()
 run = putStrLn "Hello, world!"
-
-testBfs :: Int -> [(Int, Int)]
-testBfs x = bfs sampleGraph x
