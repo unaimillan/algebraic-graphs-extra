a3ae5965fa3ee43b442bc3d981c3231b4c5026b2
Dantara <supercreed2@gmail.com>
2020-06-17 18:41:11 +0300

    Components improve

--- src/Algebra/Graph/Algorithm/Internal.hs
+++ src/Algebra/Graph/Algorithm/Internal.hs
@@ -1,30 +1,38 @@
 module Algebra.Graph.Algorithm.Internal where
 
-import           Algebra.Graph
+import                            Algebra.Graph
+import Data.DisjointSet           (DisjointSet)
 import qualified Data.DisjointSet as DS
-import qualified Data.Set         as S
 
 -- | Extract disjoint set of connectivity components from the graph
-components :: Ord a => Graph a -> DS.DisjointSet a
+--
+-- >>> DS.pretty $ components ((1 * 2) + (3 * 4))
+-- "{{1,2},{3,4}}"
+--
+-- >>> DS.pretty $ components (((1 * 2) + (3 * 4)) + (2 * 3))
+-- "{{1,2,3,4}}"
+components :: Ord a => Graph a -> DisjointSet a
 components Empty         = DS.empty
 components (Vertex x)    = DS.singleton x
-components (Overlay x y) = mergeComponents (components x) (components y)
-components (Connect x y) = DS.fromLists [vertexList x ++ vertexList y]
+components (Connect x y) = DS.fromLists [extractVertices x <> extractVertices y]
+components (Overlay x y) = (components x) <> (components y)
 
--- | Merge two disjoint sets
-mergeComponents ::
-     Ord a => DS.DisjointSet a -> DS.DisjointSet a -> DS.DisjointSet a
-mergeComponents x y = foldr insertComponent x (DS.toSets y)
-
--- | Insert a set into the other disjoint set structure:
--- Firstly, we find all intersections between the given set `x` and any of
--- disjoint ones. Then we unite all the intersecting sets into one big union
--- and finally append this big union to all other sets, with which we were not
--- intersected.
-insertComponent :: Ord a => S.Set a -> DS.DisjointSet a -> DS.DisjointSet a
-insertComponent x dsu =
-  DS.fromLists (S.toList (S.unions (x : connected)) : disconnected)
+-- | \( O(s) \).
+--
+-- This function extracts all vertices from a given graph.
+-- It is alternative version of VertexList function
+-- but it does not care about duplicates and has better complexity.
+--
+-- >>> extractVertices ((1 * 2) + (3 * 4))
+-- [1,2,3,4]
+extractVertices :: Graph a -> [a]
+extractVertices Empty = []
+extractVertices (Vertex v) = [v]
+extractVertices (Connect l r) = left <> right
   where
-    sets = DS.toSets dsu
-    connected = filter (not . S.disjoint x) sets
-    disconnected = map S.toList (filter (S.disjoint x) sets)
+    left = extractVertices l
+    right = extractVertices r
+extractVertices (Overlay l r) = left <> right
+  where
+    left = extractVertices l
+    right = extractVertices r
