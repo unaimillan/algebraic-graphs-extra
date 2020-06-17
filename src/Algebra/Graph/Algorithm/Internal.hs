c3ab59f1bb43a43d534744513fe090dccc56d51e
Dantara <supercreed2@gmail.com>
2020-06-17 23:29:42 +0300

    Minor fixes

--- src/Algebra/Graph/Algorithm/Internal.hs
+++ src/Algebra/Graph/Algorithm/Internal.hs
@@ -2,37 +2,17 @@ module Algebra.Graph.Algorithm.Internal where
 
 import                            Algebra.Graph
 import Data.DisjointSet           (DisjointSet)
-import qualified Data.DisjointSet as DS
+import qualified Data.DisjointSet as DisjointSet
 
 -- | Extract disjoint set of connectivity components from the graph
 --
--- >>> DS.pretty $ components ((1 * 2) + (3 * 4))
+-- >>> DisjointSet.pretty $ components ((1 * 2) + (3 * 4))
 -- "{{1,2},{3,4}}"
 --
--- >>> DS.pretty $ components (((1 * 2) + (3 * 4)) + (2 * 3))
+-- >>> DisjointSet.pretty $ components (((1 * 2) + (3 * 4)) + (2 * 3))
 -- "{{1,2,3,4}}"
 components :: Ord a => Graph a -> DisjointSet a
-components Empty         = DS.empty
-components (Vertex x)    = DS.singleton x
-components (Connect x y) = DS.fromLists [extractVertices x <> extractVertices y]
-components (Overlay x y) = (components x) <> (components y)
-
--- | \( O(s) \).
---
--- This function extracts all vertices from a given graph.
--- It is alternative version of VertexList function
--- but it does not care about duplicates and has better complexity.
---
--- >>> extractVertices ((1 * 2) + (3 * 4))
--- [1,2,3,4]
-extractVertices :: Graph a -> [a]
-extractVertices Empty = []
-extractVertices (Vertex v) = [v]
-extractVertices (Connect l r) = left <> right
-  where
-    left = extractVertices l
-    right = extractVertices r
-extractVertices (Overlay l r) = left <> right
-  where
-    left = extractVertices l
-    right = extractVertices r
+components Empty           = DisjointSet.empty
+components (Vertex x)      = DisjointSet.singleton x
+components g@(Connect _ _) = DisjointSet.singletons $ vertexSet g
+components (Overlay x y)   = components x <> components y
