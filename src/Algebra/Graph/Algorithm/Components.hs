aa121465e94da3fcbc6255e4e0713d7835913428
Dantara <supercreed2@gmail.com>
2020-06-25 19:11:08 +0300

    Regrouping files

---
+++ src/Algebra/Graph/Algorithm/Components.hs
@@ -0,0 +1,1 @@
+module Algebra.Graph.Algorithm.Components where
---
+++ src/Algebra/Graph/Algorithm/Components/DisjointSet.hs
@@ -0,0 +1,18 @@
+module Algebra.Graph.Algorithm.Components.DisjointSet where
+
+import           Algebra.Graph
+import           Data.DisjointSet (DisjointSet)
+import qualified Data.DisjointSet as DisjointSet
+
+-- | Extract disjoint set of connectivity components from the graph
+--
+-- >>> DisjointSet.pretty $ components ((1 * 2) + (3 * 4))
+-- "{{1,2},{3,4}}"
+--
+-- >>> DisjointSet.pretty $ components (((1 * 2) + (3 * 4)) + (2 * 3))
+-- "{{1,2,3,4}}"
+components :: Ord a => Graph a -> DisjointSet a
+components Empty           = DisjointSet.empty
+components (Vertex x)      = DisjointSet.singleton x
+components g@(Connect _ _) = DisjointSet.singletons $ vertexSet g
+components (Overlay x y)   = components x <> components y
